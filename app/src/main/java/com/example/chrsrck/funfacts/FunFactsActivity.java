4693a1b3e96195414e0bc36bda3552aecd613d96
chrsrck <chrsrck292@gmail.com>
2015-06-06 23:48:01 -0400

    Trying out another commit and push method within Android Studio and almost finished with tutorial.

--- app/src/main/java/com/example/chrsrck/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/chrsrck/funfacts/FunFactsActivity.java
@@ -1,38 +1,41 @@
 package com.example.chrsrck.funfacts;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
+
+    private FactBook mFactBook = new FactBook();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
-    }
+        // declare our view variables and assign the views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String fact = mFactBook.getFact();
+                //Update the label with out dynamic fact
+                factLabel.setText(fact);
+                RelativeLayout appLayout = (RelativeLayout) findViewById(R.id.appLayout);
+                appLayout.setBackgroundColor(Color.RED);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 }
