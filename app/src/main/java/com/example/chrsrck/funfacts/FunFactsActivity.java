f98ac92053571af73a0149918934048e905c9603
chrsrck <chrsrck292@gmail.com>
2015-06-08 13:31:00 -0400

    Trying out another commit and push method within Android Studio and almost finished with tutorial.

--- app/src/main/java/com/example/chrsrck/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/example/chrsrck/funfacts/FunFactsActivity.java
@@ -17,6 +17,7 @@ import java.util.Random;
 public class FunFactsActivity extends Activity {
 
     private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +25,19 @@ public class FunFactsActivity extends Activity {
         setContentView(R.layout.activity_fun_facts);
         // declare our view variables and assign the views from the layout file
         final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout appLayout = (RelativeLayout) findViewById(R.id.appLayout);
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String fact = mFactBook.getFact();
+                int color = mColorWheel.getColor();
+
                 //Update the label with out dynamic fact
                 factLabel.setText(fact);
-                RelativeLayout appLayout = (RelativeLayout) findViewById(R.id.appLayout);
-                appLayout.setBackgroundColor(Color.RED);
+                appLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
             }
         };
         showFactButton.setOnClickListener(listener);
